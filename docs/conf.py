b810dca7d01b3104673fb45c8fc4b10e7d57f363
Ouissam Benalla <ouissambenalla.ia@gmail.com>
2025-05-21 13:02:06 +0100

    jidnj

---
+++ docs/conf.py
@@ -0,0 +1,33 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../chemin_vers_ton_code'))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Cheating for computer vision'
+copyright = '2025, BENALLA Ouissam'
+author = 'BENALLA Ouissam'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc']
+
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'fr'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
