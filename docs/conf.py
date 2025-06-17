9d914b0e20764b9667a37532fce2404534559004
Ouissam Benalla <ouissambenalla.ia@gmail.com>
2025-06-17 13:24:17 +0100

    Add docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,17 +1,16 @@
 # docs/conf.py
 html_theme = 'sphinx_rtd_theme'
-html_theme_options = {
-    'navigation_depth': 4,
-    'collapse_navigation': False,
-    'sticky_navigation': True,
-    'includehidden': True,
-    'titles_only': False
-}
+html_style = None  # Désactive tout style alternatif
 
-# Ajoutez ces extensions
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'sphinx_rtd_theme'  # Nouvelle ligne importante
+    'sphinx_rtd_theme',  # Doit être explicitement déclaré
+    # Vos autres extensions...
 ]
+
+extensions = ['myst_parser']
+
+html_theme_options = {
+    'analytics_id': 'G-XXXXXX',  # Optionnel
+    'style_external_links': True,
+    'collapse_navigation': False  # Important pour la sidebar
+}
