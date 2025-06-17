e9502225b7814c9bc7778d8bece89d73736d80ef
Ouissam Benalla <ouissambenalla.ia@gmail.com>
2025-06-17 15:43:26 +0100

    /v

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '',
-    LANGUAGE: 'fr',
+    VERSION: '1.0.0',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,21 @@
-project = "Cheating-for-computer-vision"
-extensions = ["sphinx_rtd_theme"]
-html_theme = "sphinx_rtd_theme"
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+# Informations du projet
+project = 'Nom de ton projet'
+author = 'Ton nom'
+release = '1.0.0'
+
+# Extensions Sphinx
+extensions = []
+
+# Fichiers et dossiers à exclure
+exclude_patterns = []
+
+# Thème Furo
+html_theme = 'furo'
+
+# Dossiers pour les templates
+templates_path = ['_templates']
+html_static_path = ['_static']
