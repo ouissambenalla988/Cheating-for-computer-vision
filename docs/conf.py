684995f69a753faef3f7ee5515588d8e3ae4d15c
Ouissam Benalla <ouissambenalla.ia@gmail.com>
2025-06-17 13:10:40 +0100

    Switch to RTD theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,32 +1,17 @@
-# Configuration file for the Sphinx documentation builder.
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../..'))
+# docs/conf.py
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'navigation_depth': 4,
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'includehidden': True,
+    'titles_only': False
+}
 
-project = 'Computer Vision Cheating Detection System'
-copyright = '2024, Your Name'
-author = 'Your Name'
-
-release = '0.1.0'
-
+# Ajoutez ces extensions
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
-    'sphinx.ext.imgmath',
+    'sphinx_rtd_theme'  # Nouvelle ligne importante
 ]
-
-templates_path = ['_templates']
-exclude_patterns = []
-
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
-# The suffix of source filenames.
-source_suffix = '.rst'
-
-# The master toctree document.
-master_doc = 'index'
